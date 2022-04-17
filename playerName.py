8037f3743a204f63483c8f082a49525ecc4b7435
Hyunwook <hyunwook5636@gmail.com>
2022-04-17 01:05:11 -0700

    finishing search implementation

--- playerName.py
+++ playerName.py
@@ -8,3 +8,33 @@ def getPlayerTable():
         dataSet.append(data.to_dict())
 
     return dataSet
+
+def searchPlayerByName(playerList, playerName):
+    playerData = getPlayerTable()
+    searchData = list()
+
+    for player in playerData:
+        if player['player_name'] == playerName:
+            searchData.append(player)
+    
+    return searchData
+
+def searchPlayerByDraftYear(playerList, playerDraftYear):
+    playerData = getPlayerTable()
+    searchData = list()
+
+    for player in playerData:
+        if player['draft_year'] == playerDraftYear:
+            searchData.append(player)
+
+    return searchData
+
+def searchPlayerBySeason(playerList, playerSeason):
+    playerData = getPlayerTable()
+    searchData = list()
+
+    for player in playerData:
+        if player['season'] == playerSeason:
+            searchData.append(player)
+    
+    return searchData
