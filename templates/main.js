853822bc4f0759511a2d8dd4186f59e2b160fd2a
Nathan Nguyen <natenguyen2017@gmail.com>
2022-04-18 05:06:14 -0700

    Updated gui of webpage

---
+++ templates/main.js
@@ -0,0 +1,26 @@
+function showSearchName() {
+    var x = document.getElementById("searchName");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function showSearchDraftYear() {
+    var x = document.getElementById("searchDraftYear");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function showSearchSeason() {
+    var x = document.getElementById("searchSeason");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
