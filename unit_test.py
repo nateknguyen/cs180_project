7351868b746a373d3dbeda7f72587608d7ca648c
imonr001 <imonr001@ucr.edu>
2022-04-18 11:16:22 -0700

    unit Test 1

---
+++ unit_test.py
@@ -0,0 +1,40 @@
+import unittest
+from unittest import result
+import playerName
+import server
+from flask import redirect, Flask, request, render_template
+
+class UnitTest(unittest.TestCase):
+    def testSearchPlayerByName(self):
+     searchData = list()
+     result = list()
+     playerList = playerName.getPlayerTable()    
+     result = playerName.searchPlayerByName(playerList,"Travis Knight")
+     for player in playerList:
+      if player['player_name'] == "Travis Knight":
+       searchData.append(player)
+     self.assertEqual(result,searchData)
+
+    def testSearchPlayerByDraftYear(self):
+     searchData = list()
+     result = list()
+     playerList = playerName.getPlayerTable()    
+     result = playerName.searchPlayerByDraftYear(playerList,"2000")
+     for player in playerList:
+       if player['draft_year'] == "2000":
+        searchData.append(player)
+     self.assertEqual(result,searchData)
+    
+    def testSearchPlayerBySeason(self):
+     searchData = list()
+     result = list()
+     playerList = playerName.getPlayerTable()    
+     result = playerName.searchPlayerBySeason(playerList,"2016-17")
+     for player in playerList:
+       if player['season'] == "2016-17":
+        searchData.append(player)
+     self.assertEqual(result,searchData)
+ 
+
+if __name__ == '__main__':
+    unittest.main()
