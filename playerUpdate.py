fab2a4429f380a08d74365567f7afb0e1212558a
Justin <justin.trieu48@gmail.com>
2022-05-08 18:51:04 -0700

    Completed playerUpdate

--- playerUpdate.py
+++ playerUpdate.py
@@ -5,12 +5,6 @@ import json
 
 #update player data to csv
 def updatePlayer(playerList):
-    playerData = playerName.getPlayerListFromJSON()
-    searchData = list()
-
-
-    for player in playerData:
-        if player['player_name'] == playerName:
-            searchData.append(player)
+    playerData = playerName.getPlayerListFromJSON()        
     
-    return searchData
+    return playerData
