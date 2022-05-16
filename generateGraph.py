4b0dbb8f3165d2f94b4b122cedb002ac0f149d9c
Hyunwook <hyunwook5636@gmail.com>
2022-05-16 11:36:50 -0700

    improve graphs

--- generateGraph.py
+++ generateGraph.py
@@ -1,6 +1,7 @@
 import mpld3
 import matplotlib.pyplot as plt
 from decimal import Decimal
+import json
 
 def generateGraph(players):
     x = list()
@@ -17,3 +18,21 @@ def generateGraph(players):
     result = mpld3.fig_to_html(graph, figid="AGE_VS_PERFORMANCE")
 
     return result
+
+def generateScatter(players):
+    x = list()
+    y = list()
+    for player in players:
+        x.append(Decimal(player['draft_number']))
+        y.append(Decimal(player['net_rating']))
+
+    graph = plt.figure()
+    plt.scatter(x,y)
+    plt.xlabel("Draft Number", labelpad=7, fontdict={'family': 'serif', 'color': 'b', 'weight': 'bold', 'size':15})
+    plt.ylabel("Net Rating", labelpad=15, fontdict={'family': 'serif', 'color': 'r', 'weight': 'bold', 'size':15})
+
+    result = mpld3.fig_to_html(graph, figid="DRAFT_ROUND_GRAPH")
+
+    return result
+
+
